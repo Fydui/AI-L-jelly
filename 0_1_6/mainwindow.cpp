f28c8cc6df48b72a7874c681678d7fc9683c36e4
Fydui <gtx980sli@yahoo.com>
2016-06-26 22:57:02 +0800

    暑假快到了,又到了开新坑的季节 聊天机器人凉果冻-"你好~人类~" 未完成= = 尽量填坑......

---
+++ 0_1_6/mainwindow.cpp
@@ -0,0 +1,44 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    setWindowTitle(tr("凉果冻"));
+    QString str = "你好啊~人类0v0~";
+    ui->textBrowser->append(str);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString O = ui->textEdit->toPlainText();
+    QString x = "";
+    if(O == x)
+    {
+        QString str = "\n你想说啥你到是说啊,喵=v=";
+        ui->textBrowser->append(str);
+    }
+    else
+    {
+        on_textEdit_textChanged();
+    }
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    exit(0);
+}
+
+
+void MainWindow::on_textEdit_textChanged()
+{
+    QString O = ui->textEdit->toPlainText();
+
+}
---
+++ 0_1_6/mainwindow.h
@@ -0,0 +1,35 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtWidgets/QTextEdit>
+#include <QString>
+#include <iostream>
+using namespace std;
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+    //void Text_in();
+
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
+    void on_textEdit_textChanged();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+#endif // MAINWINDOW_H
