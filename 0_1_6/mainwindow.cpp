ad158389f642b7932458308820a44ffcc1e4e221
Fydui <gtx980sli@yahoo.com>
2016-06-28 23:03:13 +0800

    QAQ

--- 0_1_6/mainwindow.cpp
+++ 0_1_6/mainwindow.cpp
@@ -1,6 +1,11 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+//sqlite3* conn = NULL;
+//char* sql = "AI.db";
+
+map<QString,string>QAA;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -9,12 +14,22 @@ MainWindow::MainWindow(QWidget *parent) :
     setWindowTitle(tr("凉果冻"));
     QString str = "你好啊~人类0v0~";
     ui->textBrowser->append(str);
-}
 
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::map()
+{
+
+    QAA["你好"] = "你好啊";
+    QAA["你叫啥"] = "凉果冻";
+
+
+}
+
 
 void MainWindow::on_pushButton_clicked()
 {
@@ -40,35 +55,132 @@ void MainWindow::on_pushButton_2_clicked()
 
 void MainWindow::User_in()
 {
-    QString O = ui->textEdit->toPlainText();    //获取用户输入
-    string str = O.toStdString();
 
-    map<string,QString>QAA;     //新建哈希表
-    QString A = QAA["你好"];
-    QString B = QAA["你叫啥"];
-    QAA["你好啊"] = A;
-    QAA["凉果冻"] = B;
-
-
-
-    int t = 1;
-    while (t) {
-
-        map<string,QString>::iterator iter;
-        iter = QAA.find(str);
+    while(1)
+    {
+        QString userin = ui->textEdit->toPlainText();    //获取用户输入
+        map<QString,string>::iterator iter;
+        iter = QAA.find(userin);
         if(iter != QAA.end())
         {
-            ui->textBrowser->append();
-            t =0;
+            ui->textBrowser->append(userin);
         }
         else
         {
-            ui->textBrowser->append("\n你说啥你说啥(｡•ˇ‸ˇ•｡)我听不懂呀~");
-            ui->textBrowser->append("我现在还很笨,你可以教我说话哦╰(￣▽￣)╭\n");
-            t = 0;
+            ui->textBrowser->append("未找到");
         }
     }
-
-
-
 }
+
+
+/*
+SQLITE_API int MainWindow::Sqlite3_Open()
+{
+    const char *filename = NULL;   // Database filename (UTF-8)
+    sqlite3 **conn = NULL;          // OUT: SQLite db handle
+    return SQLITE_API int();
+}
+
+void MainWindow::Open_DB(char* sql)		//打开数据库
+{
+    char* err_msg = NULL;
+    // char sql[200] = ""; //这东西干啥的我也不清楚==暂时注释掉
+
+    // 打开数据库, 创建连接
+    if (sqlite3_open(sql, &conn) != SQLITE_OK)
+    {
+        const char* sq =sqlite3_errmsg(conn);
+        QString str = QString(QLatin1String(sq));
+        ui->textBrowser->append("无法打开数据库,错误代码:");
+        ui->textBrowser->append(str);
+
+    }
+    else
+    {
+        ui->textBrowser->append("成功打开数据库~");
+    }
+}
+
+void MainWindow::Close_DB()	//关闭数据库
+{
+    char* err_msg = NULL;
+    //char sql[200] = ""; //这东西干啥的我也不清楚==暂时注释掉
+
+    // 关闭连接。
+    if (sqlite3_close(conn) != SQLITE_OK)
+    {
+        const char* sq =sqlite3_errmsg(conn);
+        QString str = QString(QLatin1String(sq));
+        ui->textBrowser->append("无法关闭数据库,错误代码:");
+        ui->textBrowser->append(str);
+        exit(-1);
+    }
+    else
+    {
+        ui->textBrowser->append("成功关闭数据库~");
+    }
+}
+
+void MainWindow::ExecSQL(char * sql)	//写完用这个添加数据库
+{
+    sqlite3_exec(conn, sql, UserResult, 0, 0);
+}
+
+char** MainWindow::SrawQuery(char * sql, int * row, int * column, char ** result)
+{
+    sqlite3_get_table(conn, sql, &result, row, column, 0);
+    return result;
+}
+
+int MainWindow::UserResult(void *NotUsed, int argc, char **argv, char **azColName)	//将Selectuserd建造出来的数据输出存储
+{
+
+    //char* Arg_s[15];
+    //char* Lv[2];
+    for (int i = 0; i < argc; i++)
+    {
+        //cout << azColName[i] << " = " << (argv[i] ? argv[i] : "NULL") << ". ";
+        //Arg_s[i] = (argv[i] ? argv[i] : "NULL");
+        //RetBu(Arg_s[i]);	//将属性传递出去
+        //cout <<  Arg_s[i] << endl;
+    }
+    //Gcom::Save(Arg_s);
+
+    cout << endl;
+
+    return 0;
+}
+
+int MainWindow::SelectUser(int bu) //调出数据函数
+{
+
+    Open_DB(sql);
+
+    char* cErrMsg = "";
+    char s[4] = "";
+    sprintf_s(s,"%d",bu);
+    s[2] = ';';
+    char d[100] = "SELECT * FROM SG WHERE ID = ";
+    strcat_s(d, s);
+    //cout << s;
+
+    int res = sqlite3_exec(conn,d, UserResult, NULL, &cErrMsg);	//调用sqlite3_exec.直接往里输sql命令就好啦~
+
+    if (res != SQLITE_OK)
+    {
+        //const char* sq =sqlite3_errmsg(conn);
+        QString str = QString(QLatin1String(cErrMsg));
+        ui->textBrowser->append("查询数据库失败:");
+        ui->textBrowser->append(str);
+        //cout << "查询失败: " << cErrMsg << endl;
+        Close_DB();
+        return false;
+    }
+    else
+    {
+        //
+    }
+    Close_DB();
+    return 0;
+}
+*/
--- 0_1_6/mainwindow.h
+++ 0_1_6/mainwindow.h
@@ -4,7 +4,11 @@
 #include <QMainWindow>
 #include <QtWidgets/QTextEdit>
 #include <QString>
+#include <string>
 #include <iostream>
+#include <map>
+//#include "sqlite3.h"
+//#include "sqlite3ext.h"
 using namespace std;
 
 namespace Ui {
@@ -22,11 +26,21 @@ public:
     //void Text_in();
     //void User_in();
 
+
 private slots:
     void on_pushButton_clicked();
-
     void on_pushButton_2_clicked();
+
     void User_in();
+    void map();
+    /*void Open_DB(char* sql);
+    void Close_DB();
+    void ExecSQL(char *sql);
+    char **SrawQuery(char *sql, int *row, int *column, char **result);
+    int UserResult(void *NotUsed, int argc, char **argv, char **azColName);
+    SQLITE_API int Sqlite3_Open();
+    int SelectUser(int bu);*/
+
 
 private:
     Ui::MainWindow *ui;
