81b2e2b6fca03832132a61386469a3aed0202d15
Fydui <gtx980sli@yahoo.com>
2016-07-08 22:28:19 +0800

    修复Failed to load platform plugin "windows"

--- 0_1_6/main.cpp
+++ 0_1_6/main.cpp
@@ -4,9 +4,20 @@ QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE"); //添加数据库驱动�
 int main(int argc, char *argv[])
 {
 
+    QTextCodec *xcodec = QTextCodec::codecForLocale() ;
+        QString exeDir = xcodec->toUnicode( QByteArray(argv[0]) ) ;
+        QString BKE_CURRENT_DIR = QFileInfo(exeDir).path() ;
+        QStringList  libpath;
+        libpath << BKE_CURRENT_DIR+QString::fromLocal8Bit("/plugins/platforms");
+        libpath << BKE_CURRENT_DIR <<BKE_CURRENT_DIR+QString::fromLocal8Bit("/plugins/imageformats");
+        libpath << BKE_CURRENT_DIR+QString::fromLocal8Bit("/plugins");
+        libpath << QApplication::libraryPaths();
+        QApplication::setLibraryPaths(libpath) ;
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
 
 
     return a.exec();
